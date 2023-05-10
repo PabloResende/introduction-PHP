28b4cc23eda7538c5b86652af025c1df2464cf40
WadeMcfild <126430073+WadeMcfild@users.noreply.github.com>
2023-05-10 00:38:51 -0300

    ex005

--- ex005/conversor.php
+++ ex005/conversor.php
@@ -7,20 +7,20 @@
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
-    <link rel="stylesheet" href="sytle.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <main>
-        <h1>Conversão para dólar:</h1>
-        <?php
+    <h1>Conversão para dólar:</h1>
+    <?php
         $numero = $_GET["converter"];
         $numero_convertido = $numero / 4.99;
         $numero_formatado = number_format($numero_convertido, 2);
         echo "$numero convertido para dólar é \$$numero_formatado
         <br/><br/>
-        (valor fixo de conversão $4,99)";
-        ?>
-            <p><a href="javascript:history.go(-1)">Voltar a página anterior</a></p>
+        (valor fixo de conversão $4.99)";
+    ?>
+    <p><a href="javascript:history.go(-1)">Voltar a página anterior</a></p>
     </main>
 </body>
 </html>
