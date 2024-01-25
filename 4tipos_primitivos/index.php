e44249bbc84e691d59050c9564cdf758cf63b918
PabloResende <wademcfild@gmil.com>
2024-01-25 08:52:26 -0300

    ...

--- 4tipos_primitivos/index.php
+++ 4tipos_primitivos/index.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Tipos Primitivos</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-</head>
-<body>
-    <h1>testes de tipos primitivos</h1>
-    <!-- <h3>velocidade da luz</h3> -->
+
+    <!-- velocidade da luz -->
+    
     <?php 
     $valor = 300;
     $valor = 3e8; //isso é igual 3x10 elevado á 8
@@ -27,8 +17,6 @@
 
     echo "\u{1F418}"
     ?>
-</body>
-</html>
 
 
 
