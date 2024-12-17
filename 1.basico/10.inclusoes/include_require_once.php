3123bcb0a2e4deba2c9a8037bd93a3d4b091c050
Pablo Silva Carrilho de Resende <126430073+PabloResende@users.noreply.github.com>
2024-12-17 13:12:58 -0300

    ...

---
+++ 1.basico/10.inclusoes/include_require_once.php
@@ -0,0 +1,3 @@
+<!-- o include e o require sao usados para incluir arquivos, a diferenca eh que o include nao da erro se o arquivo nao for encontrado e o require da erro -->
+
+<!-- também têm o require_once e o include_once, que fazem a mesma coisa que o include e o require, mas verificam se o arquivo foi incluido antes -->
