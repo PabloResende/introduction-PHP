2fdf05018d79a5487528948da99c745f46c86eb7
Pablo Silva Carrilho de Resende <126430073+PabloResende@users.noreply.github.com>
2024-12-17 12:21:02 -0300

    ...

---
+++ 1.basico/9.estrutura_de_repeticao/do_while.php
@@ -0,0 +1,10 @@
+<?php
+
+// Do while é mais lógico, porém ele complica mais o código e acaba invertendo um pouco a lógica;
+
+$j= 0;
+
+do { // faça...
+    echo "J = $j <br>";
+    $j++;
+} while($j < 10); // enquanto...
