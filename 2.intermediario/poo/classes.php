b2bbbbf09ba20421b47c3530f669a7c2f22c8993
Pablo Silva Carrilho de Resende <126430073+PabloResende@users.noreply.github.com>
2025-01-09 14:00:29 -0300

    ...

---
+++ 2.intermediario/poo/classes.php
@@ -0,0 +1,17 @@
+<!-- No PHP, a classe é uma estrutura de dados que permite agrupar variáveis e métodos em um conjunto, com o objetivo de facilitar a manipulação de dados e a reutilização de código 
+ 
+Classes começam com letra maiuscula-->
+
+
+<?php
+
+class User {
+
+}
+
+class Programador {
+
+    // Propriedades
+    // Métodos
+} 
+// dessa forma, o php já entende que isso é uma classe, mesmo estando sem nenhum método ou propriedade dentro dela
